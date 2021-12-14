aa34e42c5ee68f355ab16b3b56b2a71bcaca54f7
Ally Racho <55199345+allyracho@users.noreply.github.com>
2021-12-14 16:57:05 -0500

    Update q1.py

--- final/q1.py
+++ final/q1.py
@@ -1,1 +1,19 @@
- 
+from mrjob.job import MRJob
+
+
+class q1(MRJob):
+    def mapper(self, key, line):
+        
+        for i in line:
+            neighbors = 0
+            if i(1) > 1000:
+                neighbors += 1
+                yield(i, neeighbors)
+                
+    def reducer(self, key, total):
+         yield key, sum(total) > 5
+  
+
+	
+if __name__ == '__main__':
+    q1.run()
