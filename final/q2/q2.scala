ff1a3394e9ff007aa462de38931eba67a3591b75
Ally Racho <55199345+allyracho@users.noreply.github.com>
2021-12-14 17:51:22 -0500

    Update q2.scala

--- final/q2/q2.scala
+++ final/q2/q2.scala
@@ -3,14 +3,15 @@ import org.apache.spark.SparkContext
 import org.apache.spark.SparkContext._
 
 
-object Q3{
+object Q2{
      def main(args: Array[String]) = {
           val conf = new SparkConf().setAppName("q2")
           val sc = new SparkContext(conf)
           val fb = sc.textFile("hdfs:///ds410/facebook").map{x => x.split("\t")}
-          val kvrdd = fb.map{x => (x(0).toInt, x(1).toInt.filter(_ > 1000))}
+          val kvrdd = fb.map{x => (x(0).toInt, x(1).toInt))}
           val grouped = kvrdd.groupByKey
-          val count = grouped.map{i => i._1, i._2.size.filter(_ > 5)}
+          val filtered = grouped.map{i => (i._1, i._2.filter(_ > 1000))}
+          val counts = filtered.map{i => (i._1, i._2.size)}
           result.saveAsTextFile("hdfs:///user/axr402/q2")
      }
 }
