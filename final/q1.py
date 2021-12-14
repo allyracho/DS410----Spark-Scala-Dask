6c246b8ae1ae333b8d334fbf782a017aa90bab37
Ally Racho <55199345+allyracho@users.noreply.github.com>
2021-12-14 18:28:32 -0500

    Update q1.py

--- final/q1.py
+++ final/q1.py
@@ -3,15 +3,15 @@ from mrjob.job import MRJob
 
 class q1(MRJob):
     def mapper(self, key, line):
-        line = line.split("\t")
-        for i in line:
-            neighbors = 0
-            if int(i(1)) > 1000:
-                neighbors += 1
-                yield(i, neighbors)
+        line = line.split("\t") #split lines by tabs
+        for i in line: 
+            neighbors = 0  #initialize count of neighbors
+            if int(i[1]) > 1000:  #if neighbors greater than 1000
+                neighbors += 1  #increase neighbor count by 1
+                yield(i[0], neighbors)  #yield id, and count of neighbors above 1000 
                 
     def reducer(self, key, total):
-         yield (key, (sum(total) > 5))
+         yield (key, (sum(total) > 5)) #reduce based on id and sum total neighbors above 1000. Only show values greater than 5
   
 
 	
