88257e85f975e629173866d93dea4959d46ee1f7
Ally Racho <55199345+allyracho@users.noreply.github.com>
2021-12-14 17:36:16 -0500

    Update q1.py

--- final/q1.py
+++ final/q1.py
@@ -3,10 +3,10 @@ from mrjob.job import MRJob
 
 class q1(MRJob):
     def mapper(self, key, line):
-        
+        line = line.split("\t")
         for i in line:
             neighbors = 0
-            if i(1) > 1000:
+            if int(i(1)) > 1000:
                 neighbors += 1
                 yield(i, neighbors)
                 
