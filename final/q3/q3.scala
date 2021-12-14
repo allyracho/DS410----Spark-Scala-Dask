ff0262ab7fe1ec1e5348e38016f33706ee922fd5
Ally Racho <55199345+allyracho@users.noreply.github.com>
2021-12-14 18:01:01 -0500

    Update q3.scala

--- final/q3/q3.scala
+++ final/q3/q3.scala
@@ -9,13 +9,7 @@ object Q3{
           val sc = new SparkContext(conf)
           val fb = sc.textFile("hdfs:///ds410/facebook").map{x => x.split("\t")}
           val kvrdd = fb.map{x => (x(0).toInt, x(1).toInt)}
-          val neighbors = fb.aggregateByKey( (0, 0) )({ // init state for aggregating 
-values with same key
-                    case ((num, total), nextitem) => (num+1, total + nextitem)
-                },{
-                    case ((num, total), (nextnum, nexttotal)) => (num + nextnum, 
-total + nexttotal)
-                })
+          val neighbors = kvrdd.aggregateByKey( (0, 0) )({case ((num, total), nextitem) => (num+1, total + nextitem)},{case ((num, total), (nextnum, nexttotal)) => (num + nextnum, total + nexttotal)})
           val grouped = kvrdd.groupByKey
           val filteredx = kvrdd.filter(i(1) > i(0)).count
           val result = .... map{i => neighbors, grouped._2.size, filteredx}
