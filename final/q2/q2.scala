017141e312a23e12b8055347ce50c6d60139f285
Ally Racho <55199345+allyracho@users.noreply.github.com>
2021-12-14 18:11:39 -0500

    Update q2.scala

--- final/q2/q2.scala
+++ final/q2/q2.scala
@@ -9,9 +9,9 @@ object Q2{
           val sc = new SparkContext(conf)
           val fb = sc.textFile("hdfs:///ds410/facebook").map{x => x.split("\t")}
           val kvrdd = fb.map{x => (x(0), x(1).toInt))}
-          val grouped = kvrdd.groupByKey
-          val filtered = grouped.map{i => (i._1, i._2.filter(_ > 1000))}
-          val counts = filtered.map{i => (i._1, i._2.size)}
+          val grouped = kvrdd.groupByKey //group by keys
+          val filtered = grouped.map{i => (i._1, i._2.filter(_ > 1000))} // filter to only neighbors with ids > 1000
+          val counts = filtered.map{i => (i._1, i._2.size)} // get total count of neighbors with > 1000
           result.saveAsTextFile("hdfs:///user/axr402/q2")
      }
 }
