ec4bd8f6fff9348a2260dcbd06ad2c479715f296
Ally Racho <55199345+allyracho@users.noreply.github.com>
2021-12-14 17:03:44 -0500

    Update q2.scala

--- final/q2/q2.scala
+++ final/q2/q2.scala
@@ -1,1 +1,16 @@
- 
+import org.apache.spark.SparkConf
+import org.apache.spark.SparkContext
+import org.apache.spark.SparkContext._
+
+
+object Q3{
+     def main(args: Array[String]) = {
+          val conf = new SparkConf().setAppName("q3")
+          val sc = new SparkContext(conf)
+          val fb = sc.textFile("hdfs:///ds410/facebook").map{x => x.split("\t")}
+          val kvrdd = fb.map{x => (x(0).toInt, x(1).toInt.filter(_ < 1000))}
+          val kvrdd_filter = kvrdd.filter(
+          result.saveAsTextFile("hdfs:///user/axr402/q2")
+     }
+}
+
