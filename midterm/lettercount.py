95509d00efd226c51ccd34db6ae037f69168f637
Ally Racho <55199345+allyracho@users.noreply.github.com>
2022-04-12 19:59:01 -0400

    Update lettercount.py

--- midterm/lettercount.py
+++ midterm/lettercount.py
@@ -3,24 +3,32 @@ from mrjob.job import MRJob
 class LetterCount(MRJob):
 
     def mapper_init(self):
-        pass
+        self.cache = {}
 
     def mapper(self, key, line):
         for symbol in line:
+        self.cache[symbol] += 1
+        if ( cache size > limit):
             if symbol.isalpha():
-                yield (symbol, 1)
+                for s in cache:
+                    yield (symbol, cache[s])
+                cache.clear()
 
     def mapper_final(self):
-        pass
+        if cache is not empty:
+            for s in cache:
+                yield s, cache[s]
 
     def reducer_init(self):
-        pass
+        self.cache = {}
 
     def reducer(self, key, values):
         yield (key, sum(values))
+        
 
     def reducer_final(self):
-        pass
+        if cache is empty:
+            yield reducer
 
 if __name__ == '__main__':
     LetterCount.run()
